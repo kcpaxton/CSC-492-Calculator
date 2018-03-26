d8b6572465ebcf43ea2c97d4fcdb7506c525008d
Kyle Paxton <kyle.paxton@jacks.sdstate.edu>
2018-03-26 13:44:49 -0500

    Fixed last failed commit

--- Paxton2/Paxton2/MainPage.xaml.cs
+++ Paxton2/Paxton2/MainPage.xaml.cs
@@ -9,13 +9,19 @@ namespace Paxton2
 {
 	public partial class MainPage : ContentPage
 	{
-        Label inputDisplayLabel = new Label();
-        Label calculatedDisplayLabel = new Label();
-
         public MainPage()
-		{
+        {
+            Boolean operationAdded = false;
+            Boolean equalClicked = false;
+            List<string> equationEntries = new List<string>();
+            List<string> latestCommand = new List<string>();
+            latestCommand.Add(null);
+            latestCommand.Add(null);
 
+            Label courseNameLabel;
+            Label myNameLabel;
             Label displayLabel;
+            Label equationLabel;
             Button backspaceButton;
             Button clearEntryButton;
             Button clearButton;
@@ -31,9 +37,42 @@ namespace Paxton2
                 VerticalOptions = LayoutOptions.Center,
                 HorizontalOptions = LayoutOptions.Center
             };
+            this.Content = mainStack;
+            this.BackgroundImage = "background.jpg";
+
+            courseNameLabel = new Label
+            {
+                Text = "CSC 492 Mobile Apps",
+                TextColor = Color.White,
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Start
+            };
+            mainStack.Children.Add(courseNameLabel);
+
+            myNameLabel = new Label
+            {
+                Text = "Kyle Paxton",
+                TextColor = Color.White,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Start
+            };
+            mainStack.Children.Add(myNameLabel);
+
+            equationLabel = new Label
+            {
+                TextColor = Color.White,
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.End
+            };
+            mainStack.Children.Add(equationLabel);
 
             displayLabel = new Label
             {
+                Text = "0",
+                TextColor = Color.White,
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
                 VerticalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.End
@@ -42,6 +81,16 @@ namespace Paxton2
 
             Grid myGrid = new Grid();
             mainStack.Children.Add(myGrid);
+
+            equalsButton = new Button
+            {
+                Text = "=",
+                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
+                WidthRequest = 60,
+                IsEnabled = equationEntries.Count > 2,
+            };
+            equalsButton.Clicked += onEqualsButtonClicked;
+            myGrid.Children.Add(equalsButton, 3, 4);
 
             backspaceButton = new Button
             {
@@ -78,7 +127,8 @@ namespace Paxton2
                 Text = "/",
                 FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
                 WidthRequest = 60,
-                IsEnabled = false,
+                IsEnabled = true,
+                StyleId = "/",
             };
             divideButton.Clicked += onDivideButtonClicked;
             myGrid.Children.Add(divideButton, 3, 0);
@@ -88,7 +138,8 @@ namespace Paxton2
                 Text = "*",
                 FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
                 WidthRequest = 60,
-                IsEnabled = false,
+                IsEnabled = true,
+                StyleId = "*",
             };
             multiplyButton.Clicked += onMultiplyButtonClicked;
             myGrid.Children.Add(multiplyButton, 3, 1);
@@ -98,7 +149,8 @@ namespace Paxton2
                 Text = "-",
                 FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
                 WidthRequest = 60,
-                IsEnabled = false,
+                IsEnabled = true,
+                StyleId = "-",
             };
             subtractButton.Clicked += onSubtractButtonClicked;
             myGrid.Children.Add(subtractButton, 3, 2);
@@ -108,20 +160,13 @@ namespace Paxton2
                 Text = "+",
                 FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
                 WidthRequest = 60,
-                IsEnabled = false,
+                IsEnabled = true,
+                StyleId = "+",
             };
             addButton.Clicked += onAddButtonClicked;
             myGrid.Children.Add(addButton, 3, 3);
 
-            equalsButton = new Button
-            {
-                Text = "=",
-                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
-                WidthRequest = 60,
-                IsEnabled = false,
-            };
-            equalsButton.Clicked += onEqualsButtonClicked;
-            myGrid.Children.Add(equalsButton, 3, 4);
+            
 
             decimalButton = new Button
             {
@@ -129,6 +174,7 @@ namespace Paxton2
                 FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Button)),
                 WidthRequest = 60,
                 IsEnabled = true,
+                StyleId = ".",
             };
             decimalButton.Clicked += onDecimalButtonClicked;
             myGrid.Children.Add(decimalButton, 2, 4);
@@ -163,95 +209,303 @@ namespace Paxton2
 
                 myGrid.Children.Add(digitButton, colCounter, rowCounter);
                 colCounter++;
-                this.Content = mainStack;
-                //InitializeComponent ();
+                
             }
 
 
             void OnDigitButtonClicked(object sender, EventArgs args)
             {
+                if (displayLabel.Text == "0" || operationAdded)
+                {
+                    displayLabel.Text = "";
+                    operationAdded = false;
+                }
+
                 Button button = (Button)sender;
                 displayLabel.Text += (string)button.StyleId;
+
+                if (latestCommand.Count != 0)
+                {
+                    latestCommand.RemoveAt(latestCommand.Count - 1);
+                    latestCommand.Add(displayLabel.Text);
+                }
                 backspaceButton.IsEnabled = true;
                 clearEntryButton.IsEnabled = true;
                 clearButton.IsEnabled = true;
-                divideButton.IsEnabled = true;
-                multiplyButton.IsEnabled = true;
-                subtractButton.IsEnabled = true;
-                addButton.IsEnabled = true;
-                equalsButton.IsEnabled = true;
             }
 
             void onBackspaceButtonClicked(object sender, EventArgs args)
             {
+                clearPreviouslyCalculated();
                 string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+                
+                if (displayLabel.Text != "0")
+                {
+                    displayLabel.Text = text.Substring(0, text.Length - 1);
+                    backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+                    equalsButton.IsEnabled = equationEntries.Count > 1;
+                    if (displayLabel.Text == "")
+                    {
+                        displayLabel.Text = "0";
+                    }
+                }
             }
 
             void onClearEntryButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                clearEntryButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                displayLabel.Text = "0";
+                backspaceButton.IsEnabled = false;
+                equalsButton.IsEnabled = equationEntries.Count > 1;
             }
 
             void onClearButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                clearButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                displayLabel.Text = "0";
+                equationEntries.Clear();
+                updateEquationText();
+                backspaceButton.IsEnabled = false;
+                equalsButton.IsEnabled = false;
             }
 
             void onDivideButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                divideButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                checkFloatingDecimal();
+                Button button = (Button)sender;
+                updateEquationList((string)button.StyleId, displayLabel.Text);
+                updateLatestCommandList((string)button.StyleId, displayLabel.Text);
+                equalsButton.IsEnabled = equationEntries.Count > 1;
+                operationAdded = true;
+
             }
 
             void onMultiplyButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                multiplyButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                checkFloatingDecimal();
+                Button button = (Button)sender;
+                updateEquationList((string)button.StyleId, displayLabel.Text);
+                updateLatestCommandList((string)button.StyleId, displayLabel.Text);
+                equalsButton.IsEnabled = equationEntries.Count > 1;
+                operationAdded = true;
             }
 
             void onSubtractButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                subtractButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                checkFloatingDecimal();
+                Button button = (Button)sender;
+                updateEquationList((string)button.StyleId, displayLabel.Text);
+                updateLatestCommandList((string)button.StyleId, displayLabel.Text);
+                equalsButton.IsEnabled = equationEntries.Count > 1;
+                operationAdded = true;
             }
 
             void onAddButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                addButton.IsEnabled = displayLabel.Text.Length > 0;
-            }
-
-            void onEqualsButtonClicked(object sender, EventArgs args)
-            {
-                string text = displayLabel.Text;
-                displayLabel.Text = text.Substring(0, text.Length - 1);
-                equalsButton.IsEnabled = displayLabel.Text.Length > 0;
+                clearPreviouslyCalculated();
+                checkFloatingDecimal();
+                Button button = (Button)sender;
+                updateEquationList((string)button.StyleId, displayLabel.Text);
+                updateLatestCommandList((string)button.StyleId, displayLabel.Text);
+                equalsButton.IsEnabled = equationEntries.Count > 1;
+                operationAdded = true;
             }
 
             void onDecimalButtonClicked(object sender, EventArgs args)
             {
-                string text = displayLabel.Text;
-                Button button = (Button)sender;
-                displayLabel.Text += (string)button.StyleId;
+                clearPreviouslyCalculated();
+                if (!displayLabel.Text.Contains("."))
+                {
+                    Button button = (Button)sender;
+                    displayLabel.Text += (string)button.StyleId;
+                }
             }
 
             void onNegateButtonClicked(object sender, EventArgs args)
             {
                 Button button = (Button)sender;
-                //displayLabel.Text;
+                if(displayLabel.Text[0] == '-')
+                {
+                    displayLabel.Text = displayLabel.Text.Remove(0);
+                }
+                else
+                {
+                    displayLabel.Text = displayLabel.Text.Insert(0, "-");
+                }
             }
 
+            void onEqualsButtonClicked(object sender, EventArgs args)
+            {
+                checkFloatingDecimal();
+                operationAdded = true;
+                 
+                if (equalClicked == false)
+                {
+                    if (equationEntries.Count == 0)
+                    {
+                        equationLabel.Text = "Cannot evaluate with no entries";
+                    }
+                    else
+                    {
+                        executeCalculation();   
+                    }
+                }
+                else
+                {
+                    executeLatestCommand();
+                }
 
+            }
+
+            void updateEquationList(string currentOperation, string currentTerm)
+            {
+                equationEntries.Add(currentTerm);
+                equationEntries.Add(currentOperation);
+                updateEquationText();
+            }
+
+            void updateLatestCommandList(string currentOperation, string currentTerm)
+            {
+                //adds operation first then term 
+                // * 5
+                latestCommand.Add(currentOperation);
+                latestCommand.Add(currentTerm);
+            }
+
+            void updateEquationText()
+            {
+                equationLabel.Text = "";
+                foreach(string item in equationEntries)
+                {
+                    equationLabel.Text += item;
+                    equationLabel.Text += ' ';
+                }
+            }
+
+            void checkFloatingDecimal()
+            {
+                if(displayLabel.Text[displayLabel.Text.Length - 1] == '.')
+                {
+                    displayLabel.Text = displayLabel.Text.Substring(0, displayLabel.Text.Length - 1);
+                }
+            }
+
+            void clearPreviouslyCalculated()
+            {
+                equalClicked = false;
+                latestCommand.Clear();
+            }
+            
+            void executeCalculation()
+            {
+                int i = 0;
+                double subtotal = 0;
+                double term = 0;
+                double.TryParse(equationEntries[i], out subtotal);
+                while (i < equationEntries.Count)
+                {
+                    double.TryParse(equationEntries[i], out term);
+                    i = i + 1;
+                    if (equationEntries[i] == "*")
+                    {
+
+                        //check if there is another entry
+                        if (i + 1 < equationEntries.Count)
+                        {
+                            i = i + 1;
+                            subtotal = subtotal * double.Parse(equationEntries[i]);
+                        }
+                        else
+                        {
+                            i = i + 1;
+                            subtotal = subtotal * double.Parse(displayLabel.Text);
+                        }
+
+                    }
+                    else if (equationEntries[i] == "+")
+                    {
+                        //check if there is another entry
+                        if (i + 1 < equationEntries.Count)
+                        {
+                            i = i + 1;
+                            subtotal = subtotal + double.Parse(equationEntries[i]);
+                        }
+                        else
+                        {
+                            i = i + 1;
+                            subtotal = subtotal + double.Parse(displayLabel.Text);
+                        }
+                    }
+                    else if (equationEntries[i] == "-")
+                    {
+                        //check if there is another entry
+                        if (i + 1 < equationEntries.Count)
+                        {
+                            i = i + 1;
+                            subtotal = subtotal - double.Parse(equationEntries[i]);
+                        }
+                        else
+                        {
+                            i = i + 1;
+                            subtotal = subtotal - double.Parse(displayLabel.Text);
+                        }
+                    }
+                    else if (equationEntries[i] == "/")
+                    {
+                        //check if there is another entry
+                        if (i + 1 < equationEntries.Count)
+                        {
+                            i = i + 1;
+                            subtotal = subtotal / double.Parse(equationEntries[i]);
+                        }
+                        else
+                        {
+                            i = i + 1;
+                            subtotal = subtotal / double.Parse(displayLabel.Text);
+                        }
+                    }
+
+                }
+                equationEntries.Clear();
+                equationLabel.Text = "";
+                equalClicked = true;
+                displayLabel.Text = subtotal.ToString();
+            }
+
+            void executeLatestCommand()
+            {
+                double subtotal = 0;
+                double term = 0;
+                double.TryParse(displayLabel.Text, out subtotal);
+                if (latestCommand.Count > 0)
+                {
+                    double.TryParse(latestCommand[1], out term);
+
+                    if (latestCommand[0] == "*")
+                    {
+                        subtotal = subtotal * double.Parse(latestCommand[1]);
+                    }
+                    else if (latestCommand[0] == "+")
+                    {
+                        subtotal = subtotal + double.Parse(latestCommand[1]);
+                    }
+                    else if (latestCommand[0] == "-")
+                    {
+                        subtotal = subtotal - double.Parse(latestCommand[1]);
+                    }
+                    else if (latestCommand[0] == "/")
+                    {
+                        subtotal = subtotal / double.Parse(latestCommand[1]);
+                    }
+
+                    equationLabel.Text = "";
+                    equalClicked = true;
+                    displayLabel.Text = subtotal.ToString();
+                }
+            }
         }
     }
 }
